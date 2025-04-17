Fixed issues in the normalization and loading parts of the influence script.

RED_Influence.py
@@ -7,60 +7,69 @@ import numpy as np
 from PIL import Image
 
 from nnet.Model_Diffusion import get_diffusion
-from RED_Projection import pet_to_sino, set_projection_size
+from RED_Projection import pet_to_sino, sino_to_pet, set_projection_size
+from nnet.Model_Utility import batch_psnr, batch_ssim, batch_nrmse
 
-model_path = "path to trained DCN.pt"
-set_projection_size(360,360)
+model_path = "path to trained dcn model"
+# model_path = "./models/DCN_D20_20250412_205402/model_580000.pt"
 
+device = "cuda:0"
+set_projection_size((360,360))
 
 def load_dcm(dicom_file):
     return pydicom.dcmread(dicom_file)
     
-def calculate_suv(ds):
+def calculate_suv(pixel_array, ds):
     
-    image_data = ds.pixel_array
-    patient_weight = ds.PatientWeight  # 例如: 70.0 kg
+    image_data = pixel_array
+    patient_weight = ds.PatientWeight
     radiopharmaceutical_info = ds.RadiopharmaceuticalInformationSequence[0]
-    injected_dose = radiopharmaceutical_info.RadionuclideTotalDose  # 例如 50000000 Bq
-    activity_concentration = image_data  # 例如: 3.5 Bq/mL
+    injected_dose = radiopharmaceutical_info.RadionuclideTotalDose
+    activity_concentration = image_data
     suv = (activity_concentration * patient_weight * 1000) / injected_dose
 
     return suv
 
 def influence_RED(low_dose_sino_path = "", norm_dose_sino_path = ""):
-    global test_params
     with torch.no_grad():
 
-        diffusion = get_diffusion(path = test_params["model_path"]).to("cuda")
-
-        alpha = diffusion.alphas.to("cuda")
+        diffusion = get_diffusion(path = model_path, device = device)
 
         low_dcm = load_dcm(low_dose_sino_path)
         norm_dcm = load_dcm(norm_dose_sino_path)
 
-        img = np.clip(calculate_suv(low_dcm),0,10)
-        target = np.clip(calculate_suv(norm_dcm),0,10)
-        low_sino = pet_to_sino(img)
-        norm_sino = pet_to_sino(target)
+        low_sino = pet_to_sino(low_dcm.pixel_array)
+        norm_sino = pet_to_sino(norm_dcm.pixel_array)
 
-        low_sino = np.array(Image.fromarray(low_sino).resize((352,352)))
-        norm_sino = np.array(Image.fromarray(norm_sino).resize((352,352)))
-        
-        low_sino_tensor = torch.tensor(low_sino)[None,None,:]
-        norm_sino_tensor = torch.tensor(norm_sino)[None,None,:]
+        low_sino = calculate_suv(low_sino,low_dcm)
+        norm_sino = calculate_suv(norm_sino,norm_dcm)
 
-        max_v = low_sino_tensor.max()
-        val_low_dose_sino =  (max_v * low_sino_tensor / torch.clamp(max_v**2,min=1)).to("cuda")
-        val_norm_dose_sino = (max_v * norm_sino_tensor / torch.clamp(max_v**2,min=1)).to("cuda")
+        max_v =low_sino.max()
+        low_dose_sino =  low_sino / max_v
+        norm_dose_sino = norm_sino / max_v
 
-        pred_sino = diffusion.reverse(val_low_dose_sino, 50, need_corr= True)
+        low_sino_tensor = torch.tensor(low_dose_sino)[None,None,:].to(device)
+        norm_sino_tensor = torch.tensor(norm_dose_sino)[None,None,:].to(device)
+
+        x_T = (low_sino_tensor - 0.5 ) * 2
+        pred_sino = diffusion.reverse(x_T, 50, need_corr= True)
+
+        pred_sino = pred_sino[0][0].detach().to("cpu").numpy() / 2 + 0.5
+        pred_img = sino_to_pet(pred_sino)
+
+        from PIL import Image
+        normalize = lambda x: (x - x.min()) / (x.max() - x.min() + 1e-6)
+        Image.fromarray(255 * normalize(pred_sino)).show()
+        Image.fromarray(255 * normalize(pred_img)).show()
+
 
     return pred_sino
 
     
 if __name__ == '__main__':
-    ld_path = r"E:\dataset\uExplorer\PART3\PART3\Anonymous_ANO_20230505_1758162_120417\2.886 x 600 WB D10\00000251.dcm"
-    norm_path = r"E:\dataset\uExplorer\PART3\PART3\Anonymous_ANO_20230505_1758162_120417\2.886 x 600 WB NORMAL\00000251.dcm"
+    ld_path = r"path to low dows dcm"
+    norm_path = r"path to norm dows dcm"
+
     rec_img = influence_RED(ld_path,norm_path)
 
     print("done")

nnet/Model_Diffusion.py
@@ -40,6 +40,12 @@ class REDiffusion(nn.Module):
 
         return x_t
     
+    def to_device(self, device):
+        self.nn_ren.to(device)
+        self.nn_dcn.to(device)
+        self.alphas.to(device)
+        self.betas.to(device)
+
     def save(self, save_path:str):
         "Save model and parameters"
         
@@ -60,8 +66,8 @@ class REDiffusion(nn.Module):
 
         self.training_step  = data["training_step"]
         self.max_T          = data["max_T"]
-        self.alphas         = data["alphas"]
-        self.betas         = data["betas"]
+        self.alphas         = data["alphas"].to(self.device)
+        self.betas         = data["betas"].to(self.device)
         self.nn_ren.load_state_dict(data['nn_ren'])
         self.nn_dcn.load_state_dict(data['nn_dcn'])
 